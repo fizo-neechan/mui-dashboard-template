52fe2977029a131c2bff61bffc53dd8597c76d8d
Faizan Haq <fizoneechan@gmail.com>
2024-10-29 03:55:10 +0500

    refactors

--- src/features/admin/nav-bar/Navbar.tsx
+++ src/features/admin/nav-bar/Navbar.tsx
@@ -13,6 +13,7 @@ import ArchiveIcon from '@mui/icons-material/Archive';
 import FileCopyIcon from '@mui/icons-material/FileCopy';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { StyledMenu } from "./Navbar.style";
 
 const Navbar = () => {
   const pathname = usePathname().split('/');
@@ -43,12 +44,12 @@ const Navbar = () => {
       <NavbarMenuIcon >
         <PersonIcon />
       </NavbarMenuIcon>
-      <IconButton >
+      <NavbarMenuIcon >
         <SettingsIcon />
-      </IconButton>
-      <IconButton >
+      </NavbarMenuIcon>
+      <NavbarMenuIcon >
         <NotificationsIcon />
-      </IconButton>
+      </NavbarMenuIcon>
     </Box>
   </Box>;
 };
@@ -76,9 +77,9 @@ const NavbarMenuIcon = ({ children }: { children: React.ReactNode }) => {
       </IconButton>
 
       <StyledMenu
-        id="demo-customized-menu"
+        id={id}
         MenuListProps={{
-          'aria-labelledby': 'demo-customized-button',
+          'aria-labelledby': id,
         }}
         anchorEl={anchorEl}
         open={open}
@@ -134,46 +135,3 @@ const BreadCrumbs = ({ pathname }: { pathname: string[] }) => {
 
 export default Navbar;
 
-
-const StyledMenu = styled((props: MenuProps) => (
-  <Menu
-    elevation={0}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'right',
-    }}
-    {...props}
-  />
-))(({ theme }) => ({
-  '& .MuiPaper-root': {
-    borderRadius: 6,
-    marginTop: theme.spacing(1),
-    minWidth: 180,
-    color: 'rgb(55, 65, 81)',
-    boxShadow:
-      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-    '& .MuiMenu-list': {
-      padding: '4px 0',
-    },
-    '& .MuiMenuItem-root': {
-      '& .MuiSvgIcon-root': {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-      },
-      '&:active': {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity,
-        ),
-      },
-    },
-    ...theme.applyStyles('dark', {
-      color: theme.palette.grey[300],
-    }),
-  },
-}));
