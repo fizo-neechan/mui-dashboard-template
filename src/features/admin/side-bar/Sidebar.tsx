b3473c41e4bc718609b03f22087879eed27c7257
Faizan Haq <fizoneechan@gmail.com>
2024-10-29 03:48:47 +0500

    dynamic sidebar

--- src/features/admin/side-bar/Sidebar.tsx
+++ src/features/admin/side-bar/Sidebar.tsx
@@ -1,13 +1,72 @@
 "use client";
-import { Divider, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, MenuItem, Typography } from "@mui/material";
 import { useState } from "react";
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import { Drawer, DrawerBox, DrawerHeader } from "./Sidebar.style";
+import { usePathname, useRouter } from "next/navigation";
+
+interface MenuItem {
+  text: string;
+  icon: JSX.Element;
+  path: string;
+}
+
+interface MenuItems {
+  section: string;
+  items: MenuItem[];
+}
+
+const MENU_ITEMS: MenuItems[] = [
+  {
+    section: 'Main', items: [
+      {
+        text: 'Dashboard',
+        icon: <InboxIcon />,
+        path: '/admin',
+      },
+      {
+        text: 'Inbox',
+        icon: <InboxIcon />,
+        path: '/admin/inbox',
+      },
+      {
+        text: 'Starred',
+        icon: <InboxIcon />,
+        path: '/admin/starred',
+      },
+      {
+        text: 'Drafts',
+        icon: <InboxIcon />,
+        path: '/admin/drafts',
+      },
+    ]
+  },
+  {
+    section: 'Account', items: [
+      {
+        text: 'Your Account',
+        icon: <InboxIcon />,
+        path: '/admin/account/manage',
+      },
+      {
+        text: 'Settings',
+        icon: <InboxIcon />,
+        path: '/admin/account/settings',
+      },
+      {
+        text: 'Users',
+        icon: <InboxIcon />,
+        path: '/admin/account/users',
+      },
+    ]
+  },
+]
 
 export default function SideBar() {
+  const pathname = usePathname();
   const [open, setOpen] = useState(true);
 
   const handleDrawerOpen = () => {
@@ -24,7 +83,7 @@ export default function SideBar() {
         <DrawerHeader>
           <ListItemText
             primaryTypographyProps={{
-              fontWeight: "600 !important",
+              fontWeight: "600",
               fontSize: "1.2rem",
               color: "text.main",
               marginTop: '0.5rem'
@@ -44,10 +103,14 @@ export default function SideBar() {
             {!open ? <ChevronRightIcon /> : <ChevronLeftIcon />}
           </IconButton>
         </DrawerHeader>
-        <Divider />
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <StyledListItem index={index} text={text} open={open} />
+          {MENU_ITEMS.map((section, idx) => (
+            <>
+              <MenuSectionDivider name={section.section} open={open} />
+              {section.items.map((item, jdx) => (
+                <StyledListItem text={item.text} icon={item.icon} selected={pathname === item.path} path={item.path} open={open} />
+              ))}
+            </>
           ))}
         </List>
       </DrawerBox>
@@ -55,24 +118,57 @@ export default function SideBar() {
   );
 }
 
-interface StyledListItemProps {
-  text: string;
-  index: number;
-  open: boolean;
+const MenuSectionDivider = ({ name, open }: { name: string, open: boolean }) => {
+  return <Box sx={{
+    display: 'flex',
+    gap: '0.8rem',
+    alignItems: 'center',
+    margin: "0.8rem 0",
+    opacity: open ? 1 : 0,
+    transition: 'opacity 0.1s ease-in-out',
+  }}>
+    <Box sx={{
+      backgroundColor: 'accent.main',
+      width: '1.4rem',
+      height: '0.1rem',
+    }}></Box>
+
+    <Typography sx={{
+      color: 'accent.main',
+      fontSize: '1.2rem',
+      fontWeight: '600',
+      lineHeight: '150%',
+    }}>
+      {name}
+    </Typography>
+
+    <Box sx={{
+      backgroundColor: 'accent.main',
+      width: '100%',
+      height: '0.1rem',
+    }}></Box>
+  </Box>;
 }
 
-export const StyledListItem: React.FC<StyledListItemProps> = ({ text, index, open }) => {
+interface StyledListItemProps extends MenuItem {
+  open: boolean;
+  selected: boolean;
+}
+
+export const StyledListItem: React.FC<StyledListItemProps> = ({ text, icon, selected, open, path }) => {
+  const router = useRouter();
   return (
     <ListItem
       disablePadding
       sx={{
         display: "block",
-        backgroundColor: open && index === 0 ? "background.white" : "",
+        backgroundColor: open && selected ? "background.white" : "",
         borderRadius: "8px",
         marginBottom: "0.4rem",
       }}
     >
       <ListItemButton
+        onClick={() => { router.push(path) }}
         sx={[
           {
             minHeight: 48,
@@ -91,11 +187,11 @@ export const StyledListItem: React.FC<StyledListItemProps> = ({ text, index, ope
           sx={[
             {
               minWidth: 0,
-              color: index !== 0 ? "accent.main" : "text.light",
+              color: !selected ? "accent.main" : "text.light",
               borderRadius: "8px",
               padding: "0.5rem",
               justifyContent: "center",
-              backgroundColor: index !== 0 ? "background.white" : "accent.main",
+              backgroundColor: !selected ? "background.white" : "accent.main",
             },
             open
               ? {
@@ -106,14 +202,14 @@ export const StyledListItem: React.FC<StyledListItemProps> = ({ text, index, ope
               },
           ]}
         >
-          {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+        {icon}
         </ListItemIcon>
         <ListItemText
           primary={text}
           primaryTypographyProps={{
             fontWeight: "600 !important",
             fontSize: "1.2rem",
-            color: index !== 0 ? "text.secondary" : "text.main",
+            color: !selected ? "text.secondary" : "text.main",
           }}
           sx={[
             open
