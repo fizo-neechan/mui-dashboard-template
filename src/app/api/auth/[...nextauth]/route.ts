8bf82bc6a393a76aaf8219d3b0c9d1e4786d844f
Faizan Haq <fizoneechan@gmail.com>
2025-02-05 13:05:37 +0500

    refactor authentication and user role management; introduce UserRole enum and enhance role validation

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,72 +1,7 @@
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { compare } from 'bcrypt';
 import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
 
-import AuthError, { AuthErrorType } from '@/types/auth-error';
-import prisma from '@/utils/prisma';
+import authOptions from '@/lib/auth/auth-options';
 
-const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      id: 'credentials',
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', type: 'text' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new AuthError(AuthErrorType.MISSING_FIELDS, 400);
-        }
-        
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
-        });
-        
-        if (!user) {
-          throw new AuthError(AuthErrorType.USER_NOT_FOUND, 404);
-        }
-        if (!user.emailVerified) {
-          throw new AuthError(AuthErrorType.ACCOUNT_UNVERIFIED, 401);
-        }
-        
-        const isValidPassword = await compare(credentials.password, user.password);
-        
-        if (!isValidPassword) {
-          throw new AuthError(AuthErrorType.INVALID_CREDENTIALS, 401);
-        }
-
-        return {
-          id: user.id,
-          email: user.email,
-          name: user.name,
-          role: user.role,
-          isAdmin: user.role === 'ADMIN',
-        };
-      },
-    }),
-  ],
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: 'jwt',
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.role = user.role;
-      }
-
-      return token;
-    },
-    async session({ session, token }) {
-      session.user.role = token.role;
-
-      return session;
-    },
-  },
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
